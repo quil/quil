af894d5379e762ace38aab3f5b4cca6cee4dfc0e
Roland Sadowski <szabla@gmail.com>
2008-09-08 19:32:46 +0200

    Added CPL clause

--- rosado/processing/constants.clj
+++ rosado/processing/constants.clj
@@ -1,4 +1,13 @@
+;; Constants for the simple processing wrapper for Clojure
+;; Roland Sadowski [szabla gmail com]
 
+;; Copyright (c) 2008 Roland Sadowski. All rights reserved.  The use and
+;; distribution terms for this software are covered by the Common
+;; Public License 1.0 (http://www.opensource.org/licenses/cpl1.0.php)
+;; which can be found in the file CPL.TXT at the root of this
+;; distribution.  By using this software in any fashion, you are
+;; agreeing to be bound by the terms of this license.  You must not
+;; remove this notice, or any other, from this software. 
 
 ;; renderers known to processing.core
 
--- rosado/processing/processing.clj
+++ rosado/processing/processing.clj
@@ -1,6 +1,14 @@
 ;; simple processing wrapper for Clojure
 ;; Roland Sadowski [szabla gmail com]
 
+;; Copyright (c) 2008 Roland Sadowski. All rights reserved.  The use and
+;; distribution terms for this software are covered by the Common
+;; Public License 1.0 (http://www.opensource.org/licenses/cpl1.0.php)
+;; which can be found in the file CPL.TXT at the root of this
+;; distribution.  By using this software in any fashion, you are
+;; agreeing to be bound by the terms of this license.  You must not
+;; remove this notice, or any other, from this software. 
+
 (clojure/ns rosado.processing
 			(:import (processing.core PApplet PImage PGraphics PFont))
 			(:load-resources "constants.clj"))
