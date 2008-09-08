05344fe177c5aeaf012a6af058094777776c5f08
Roland Sadowski <szabla@gmail.com>
2008-09-08 19:36:53 +0200

    Added CPL Clause

--- rosado/io/io.clj
+++ rosado/io/io.clj
@@ -1,6 +1,14 @@
 ;; simple io abstraction layer for clojure
 ;; Roland Sadowski [szabla gmail com]
 
+;; Copyright (c) 2008 Roland Sadowski. All rights reserved.  The use and
+;; distribution terms for this software are covered by the Common
+;; Public License 1.0 (http://www.opensource.org/licenses/cpl1.0.php)
+;; which can be found in the file CPL.TXT at the root of this
+;; distribution.  By using this software in any fashion, you are
+;; agreeing to be bound by the terms of this license.  You must not
+;; remove this notice, or any other, from this software. 
+
 (clojure/ns rosado.io
 			(:import (java.io File FileReader FileWriter BufferedReader BufferedWriter
 							   FileInputStream FileOutputStream FileOutputStream
