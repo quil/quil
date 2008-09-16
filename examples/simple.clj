dca17ba3b20cc9f3e4f0ef6c9c3525aba2c95d09
Roland Sadowski <szabla@gmail.com>
2008-09-16 22:59:52 +0200

    Minor change: indendation in fancy-draw

--- examples/simple.clj
+++ examples/simple.clj
@@ -14,14 +14,14 @@
 (defn fancy-draw
   "An example of a function which does *something*."
   [dst]
-   (background-float (rand-int 256) (rand-int 256) (rand-int 256))
-   (fill-float (rand-int 125) (rand-int 125) (rand-int 125))
-   (ellipse 100 100 (rand-int 90) (rand-int 90))
-   (stroke-float 10)
-   (line 10 10 (rand-int 150) (rand-int 150))
-   (no-stroke)
-   (filter-kind INVERT)
-   (framerate 10))
+  (background-float (rand-int 256) (rand-int 256) (rand-int 256))
+  (fill-float (rand-int 125) (rand-int 125) (rand-int 125))
+  (ellipse 100 100 (rand-int 90) (rand-int 90))
+  (stroke-float 10)
+  (line 10 10 (rand-int 150) (rand-int 150))
+  (no-stroke)
+  (filter-kind INVERT)
+  (framerate 10))
 
 ;; below, we create an PApplet proxy and override setup() and draw()
 ;; methods. Then we put the applet into a window and display it.
