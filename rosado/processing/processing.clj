9ba9fc87f71ae7bcb81cc252d537056c03b40874
Roland Sadowski <szabla@gmail.com>
2008-09-28 02:17:51 +0200

    Added pmouse-x and p-mouse-y

--- rosado/processing/processing.clj
+++ rosado/processing/processing.clj
@@ -491,6 +491,10 @@
   ([fovy aspect z-near z-far]
 	 (.perspective *applet* (float fovy) (float aspect) (float z-near) (float z-far))))
 
+(defn pmouse-x [] (.pmouseX *applet*))
+
+(defn pmouse-y [] (.pmouseY *applet*))
+
 (defn point
   ([x y] (.point *applet* (float x)(float y)))
   ([x y z] (.point *applet* (float x) (float y) (float z))))
