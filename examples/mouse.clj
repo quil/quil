4f14b25ede0c01018b5aad44c54ab68582b1fd93
Roland Sadowski <szabla@gmail.com>
2009-09-17 20:38:38 +0200

    Added mouse example

---
+++ examples/mouse.clj
@@ -0,0 +1,35 @@
+;; processing example
+
+(ns mouse-example
+  (:use [rosado.processing]
+        [rosado.processing.applet]))
+
+(def mouse-position (atom [0 0]))
+
+(defn draw
+  []
+  (background-float 125)
+  (stroke-weight 20)
+  (stroke-float 10)
+  (let [[x y] @mouse-position]
+    (point x y)))
+
+(defn setup []
+  (smooth)
+  (no-stroke))
+
+
+(defn mouse-moved [evt]
+  (let [x (.getX evt) y (.getY evt)]
+    (reset! mouse-position [x y])))
+
+;; Now we just need to define an applet:
+
+(defapplet mouse-example :title "Mouse example."
+  :size [200 200]
+  :setup setup :draw draw
+  :mouse-moved mouse-moved)
+
+(run mouse-example)
+
+;; (stop mouse-example)
