4bac5fb249fa4583fa0ea79ed2126ad18b032f2a
Roland Sadowski <szabla@gmail.com>
2009-12-01 22:57:15 +0100

    clj-processing now works with leiningen

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject rosado.processing "1.0.7"
+  :description "A wrapper for Processing in Clojure."
+  :dependencies [[org.clojars.fyuryu/processing.core "1.0.7"]
+                 [org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
+  :repositories [["clojars" "http://clojars.org/repo"]])
