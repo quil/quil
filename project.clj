1ef90309c3e6835b41e9e2f6b0e383c5c94c97bd
Roland Sadowski <szabla@gmail.com>
2012-01-03 11:21:41 +0000

    update project.clj to use Clojure 1.3 by default

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject org.clojars.automata/rosado.processing "1.1.0"
   :description "A wrapper for Processing in Clojure."
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojars.fyuryu/processing.core "1.1.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojars.fyuryu/processing.core "1.1.0"]])
