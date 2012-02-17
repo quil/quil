4db132569f90d92d773267a59ab304ad001ce749
Sam Aaron <samaaron@gmail.com>
2012-02-17 00:12:10 +0000

    add opengl deps (lein2's native dep support FTW!)

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject org.clojars.automata/rosado.processing "1.1.0"
   :description "A wrapper for Processing in Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojars.fyuryu/processing.core "1.1.0"]])
+                 [org.clojars.fyuryu/processing.core "1.1.0"]
+                 [processing-jogl "1.5.1"]
+                 [processing-opengl "1.5.1"]
+                 [processing-gluegen-rt "1.5.1"]])
