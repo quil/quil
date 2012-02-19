2f829b5436c8eeebc0db66a9feb42975b56fa1c2
Sam Aaron <samaaron@gmail.com>
2012-02-19 21:05:54 +0000

    point native jars to org.clojars.processing-core clojars

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "A wrapper for Processing in Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojars.processing-core/org.processing.core "1.5.1"]
-                 [processing-jogl "1.5.1"]
-                 [processing-opengl "1.5.1"]
-                 [processing-gluegen-rt "1.5.1"]])
+                 [org.clojars.processing-core/org.processing.gluegen-rt "1.5.1"]
+                 [org.clojars.processing-core/org.processing.jogl "1.5.1"]
+                 [org.clojars.processing-core/org.processing.opengl "1.5.1"]])
