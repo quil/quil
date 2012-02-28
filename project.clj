e27a3e594f4fccfbcef5f331012d6c9c4d4ff4ad
Sam Aaron <samaaron@gmail.com>
2012-02-28 13:49:35 +0000

    Add deps required for PDF support

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject processing-core/processing.core "0.1.0-SNAPSHOT"
+(defproject processing-core/processing.core "1.0.0-SNAPSHOT"
   :description "A wrapper for Processing in Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojars.processing-core/org.processing.core "1.5.1"]
                  [org.clojars.processing-core/org.processing.gluegen-rt "1.5.1"]
                  [org.clojars.processing-core/org.processing.jogl "1.5.1"]
-                 [org.clojars.processing-core/org.processing.opengl "1.5.1"]])
+                 [org.clojars.processing-core/org.processing.opengl "1.5.1"]
+                 [org.clojars.processing-core/org.processing.itext "1.5.1"]
+                 [org.clojars.processing-core/org.processing.pdf "1.5.1"]])
