4a6cb7f82ef838795625b14fd60b5458250935d6
Sam Aaron <samaaron@gmail.com>
2012-03-07 22:49:33 +0000

    Add extra project info.

--- project.clj
+++ project.clj
@@ -1,5 +1,11 @@
 (defproject quil "1.0.0-SNAPSHOT"
   :description "(mix Processing Clojure)"
+  :url "http://github.com/quil/quil"
+  :mailing-list {:name "Quil Mailing List"
+                 :archive "https://groups.google.com/forum/?fromgroups#!forum/clj-processing"
+                 :post "clj-processing@googlegroups.com"}
+  :license {:name "Common Public License - v 1.0"
+            :url "http://www.opensource.org/licenses/cpl1.0"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojars.processing-core/org.processing.core "1.5.1"]
                  [org.clojars.processing-core/org.processing.gluegen-rt "1.5.1"]
