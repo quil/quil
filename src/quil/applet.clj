41f588b211e57b4f9373592281ec6ebc6000c033
Nikita Beloglazov <nikelandjelo@gmail.com>
2013-03-13 10:04:01 +0300

    Don't block thread until window size is setup. Quil hangs on some machine when using OpenGL renderer.

--- src/quil/applet.clj
+++ src/quil/applet.clj
@@ -127,23 +127,15 @@
                   :pdf    PApplet/PDF
                   :dxf    PApplet/DXF})
 
-(defn- wait-event-dispatch-thread
-  "Blocks current thread until all events in AWT event dispatch thread are processed."
-  []
-  (javax.swing.SwingUtilities/invokeAndWait (fn [])))
-
 (defn- applet-set-size
   ([width height]
-     (.size (current-applet) (int width) (int height))
-     (wait-event-dispatch-thread))
+     (.size (current-applet) (int width) (int height)))
   ([width height renderer]
      (let [renderer (resolve-constant-key renderer renderer-modes)]
-       (.size (current-applet) (int width) (int height) renderer))
-     (wait-event-dispatch-thread))
-    ([width height renderer path]
+       (.size (current-applet) (int width) (int height) renderer)))
+  ([width height renderer path]
      (let [renderer (resolve-constant-key renderer renderer-modes)]
-       (.size (current-applet) (int width) (int height) renderer path))
-     (wait-event-dispatch-thread)))
+       (.size (current-applet) (int width) (int height) renderer path))))
 
 (defn- validate-size!
   "Checks that the size vector is exactly two elements. If not, throws
