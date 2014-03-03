957a5aa5a746966e1425fa33e65774950fd4a55d
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-03-03 22:53:46 +0000

    Add test support for snippets.

--- test/quil/snippet.clj
+++ test/quil/snippet.clj
@@ -1,18 +1,38 @@
 (ns quil.snippet
-  (:require [quil.core :as q]))
+  (:require [quil.core :as q]
+            [clojure.test :refer [is]]))
+
+(def default-size [500 500])
 
 (def snippets (atom #{}))
+
+(defmacro snippet-as-test [& draw-fn-body]
+  `(let [result# (promise)]
+     (q/sketch
+      :size default-size
+      :draw (fn []
+              (try
+                ~@draw-fn-body
+                (catch Exception e#
+                  (println "Error" e#)
+                  (.printStackTrace e#)
+                  (deliver result# e#))
+                (finally (q/exit))))
+      :on-close #(deliver result# nil))
+     (is (nil? @result#))))
 
 (defmacro defsnippet [name opts & body]
   `(let [full-name# (symbol (str *ns* "/" '~name))
          snippet# (assoc ~opts
                     :name '~name
                     :draw-fn (fn [] ~@body))]
-     (def ~name snippet#)
+     (def ~(vary-meta name assoc
+                      :test `(fn [] (snippet-as-test ~@body)))
+       snippet#)
      (swap! snippets conj full-name#)))
 
 (defn show-snippet [snippet]
   (q/sketch
     :title (str (:name snippet))
-    :size [500 500]
+    :size default-size
     :draw (:draw-fn snippet)))
