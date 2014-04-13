08c092b483fe357fc55d55823bd2456607fe6ac3
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-04-13 22:03:15 +0100

    Add snippets for Structure category (delay-frame). Issue #87.

--- test/snippets/structure.clj
+++ test/snippets/structure.clj
@@ -11,3 +11,8 @@
   (ellipse 250 250 100 100)
   (pop-style)
   (ellipse 375 375 100 100))
+
+(defsnippet delay-s {}
+  (background 127)
+  (ellipse (* 5 (frame-count)) (* 5 (frame-count)) 50 50)
+  (delay-frame (rand-int (* 500 (rand-int 4)))))
