93e5d8775807004e60fcd4b44c3339a7b12d9dcc
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-05-18 11:09:46 +0100

    Release 2.0.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject quil "2.0.0-SNAPSHOT"
+(defproject quil "2.0.0"
   :description "(mix Processing Clojure)"
   :url "http://github.com/quil/quil"
   :mailing-list {:name "Quil Mailing List"
