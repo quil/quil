f395dedb7260dca16e300a8cccd71251ad6bad70
Norgat <norg113@gmail.com>
2014-06-02 15:09:05 +0400

    Add :features option for defsketch. Add :no-start feature option.

--- src/cljs/quil/sketch.clj
+++ src/cljs/quil/sketch.clj
@@ -5,11 +5,20 @@
      ~@body))
 
 
+(def ^{:private true} 
+	supported-features
+	#{:no-start :no-loop})
+
+
 (defmacro defsketch
   [app-name & options]
-  `(do
-     (defn ^:export ~app-name []
-       (quil.sketch/make-processing ~@options))
-
-     ~(when (not (false? (:start options)))
-     	`(quil.sketch/add-sketch-to-init-list ~app-name))))
+  (let [opts (apply hash-map options)
+        features (let [user-features (set (:features opts))]
+                   (reduce #(assoc %1 %2 (contains? user-features %2)) {}
+                           supported-features))]
+    `(do
+       (defn ^:export ~app-name []
+         (quil.sketch/make-processing ~@(apply concat (seq (merge (dissoc opts :features) features)))))
+       
+       ~(when (not (:no-start features))
+          `(quil.sketch/add-sketch-to-init-list ~app-name)))))
