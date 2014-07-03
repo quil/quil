cd22d9c5ba1ceb32071dbab09f52d5c85307dade
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-07-03 21:03:49 +0100

    Add comments to navigation-3d.

--- src/quil/middlewares/navigation_3d.clj
+++ src/quil/middlewares/navigation_3d.clj
@@ -1,15 +1,21 @@
 (ns quil.middlewares.navigation-3d
   (:require [quil.core :as q]))
 
-(defn- default-camera []
+(defn- default-position
+  "Default position configuration. Check default configuration in
+  'camera' function."
+  []
   {:position [(/ (q/width) 2.0)
               (/ (q/height) 2.0)
               (/ (q/height) 2.0 (q/tan (/ (* q/PI 60.0) 360.0)))]
    :straight [0 0 -1]
    :up [0 1 0]})
 
-; http://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
-(defn- rotate-by-axis-and-angle [v axis angle]
+(defn- rotate-by-axis-and-angle
+  "Rotates vector v by angle with axis.
+  Formula is taken from wiki:
+  http://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle"
+  [v axis angle]
   (let [[a-x a-y a-z] axis
         [x y z] v
         cs (q/cos angle)
@@ -38,37 +44,57 @@
      (+ (* d x) (* e y) (* f z))
      (+ (* g x) (* h y) (* i z))]))
 
-(defn- rotate-lr [nav-3d angle]
+(defn- rotate-lr
+  "Rotates nav-3d configuration left-right. angle positive - rotate right,
+  negative - left."
+  [nav-3d angle]
   (update-in nav-3d [:straight] rotate-by-axis-and-angle (:up nav-3d) angle))
 
-(defn- cross-product [[u1 u2 u3] [v1 v2 v3]]
+(defn- cross-product
+  "Vector cross-product: http://en.wikipedia.org/wiki/Cross_product"
+  [[u1 u2 u3] [v1 v2 v3]]
   [(- (* u2 v3) (* u3 v2))
    (- (* u3 v1) (* u1 v3))
    (- (* u1 v2) (* u2 v1))])
 
-(defn- v-mult [v mult]
+(defn- v-mult
+  "Multiply vector v by scalar mult."
+  [v mult]
   (mapv #(* % mult) v))
 
-(defn- v-plus [v1 v2]
+(defn- v-plus
+  "Sum of 2 vectors."
+  [v1 v2]
   (mapv + v1 v2))
 
-(defn- v-opposite [v]
+(defn- v-opposite
+  "Returns vector opposite to vector v."
+  [v]
   (v-mult v -1))
 
-(defn- v-normalize [v]
+(defn- v-normalize
+  "Normalize vector, returning vector
+  which has same direction but with norm equals to 1."
+  [v]
   (let [norm (->> (map q/sq v)
                   (apply +)
                   (q/sqrt))]
     (v-mult v (/ norm))))
 
-(defn- rotate-ud [nav-3d angle]
+(defn- rotate-ud
+  "Rotates nav-3d configuration up-down."
+  [nav-3d angle]
   (let [axis (cross-product (:straight nav-3d) (:up nav-3d))
         rotate #(rotate-by-axis-and-angle % axis angle)]
     (-> nav-3d
         (update-in [:straight] rotate)
         (update-in [:up] rotate))))
 
-(defn- rotate [state event pixels-in-360]
+(defn- rotate
+  "Mouse handler function which rotates nav-3d configuration.
+  It uses mouse from event object and pixels-in-360 to calculate
+  angles to rotate."
+  [state event pixels-in-360]
   (if (= 0 (:p-x event) (:p-y event))
     state
     (let [dx (- (:p-x event) (:x event))
@@ -82,7 +108,11 @@
 
 (def ^:private space (keyword " "))
 
-(defn- move [state event step-size]
+(defn- move
+  "Keyboard handler function which moves nav-3d configuration.
+  It uses keyboard key from event object to determing in which
+  direction to move."
+  [state event step-size]
   (let [{:keys [up straight]} (:navigation-3d state)]
     (if-let [dir (condp = (:key event)
                    :w straight
@@ -96,7 +126,10 @@
                  #(v-plus % (v-mult dir step-size)))
       state)))
 
-(defn- setup-3d-nav [user-setup user-settings]
+(defn- setup-3d-nav
+  "Custom 'setup' function which creates initial position
+  configuration and puts it to the state map."
+  [user-setup user-settings]
   (let [initial-state (-> user-settings
                           (select-keys [:straight :up :position])
                           (->> (merge (default-camera)))
