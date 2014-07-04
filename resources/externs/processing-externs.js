ac3edb131a9d1af4b9c43c1be0bf2cad6895e302
Norgat <norg113@gmail.com>
2014-07-04 18:42:30 +0400

    Add some bindings to Processing.js.

--- resources/externs/processing-externs.js
+++ resources/externs/processing-externs.js
@@ -1,5 +1,6 @@
 var Processing = {};
 var Processing = function(aCanvas, aCode, aFunctions){};
+
 Processing.name;
 Processing.use3DContext;
 Processing.focused;
@@ -12,7 +13,7 @@ Processing.mouseY;
 Processing.mouseButton;
 Processing.mouseScroll;
 
-    // Undefined event handlers to be replaced by user when needed
+// Undefined event handlers to be replaced by user when needed
 Processing.mouseClicked;
 Processing.mouseDragged;
 Processing.mouseMoved;
@@ -33,15 +34,15 @@ Processing.keyTyped;
 Processing.draw;
 Processing.setup;
 
-    // Remapped vars
+// Remapped vars
 Processing.__mousePressed;
 Processing.__keyPressed;
 Processing.__frameRate;
 
-    // The current animation frame
+// The current animation frame
 Processing.frameCount;
 
-    // The height/width of the canvas
+// The height/width of the canvas
 Processing.width;
 Processing.height;
 
@@ -80,3 +81,69 @@ Processing.prototype.stroke = function(arg1){};
 Processing.prototype.stroke = function(arg1, arg2){};
 Processing.prototype.stroke = function(arg1, arg2, arg3){};
 Processing.prototype.stroke = function(arg1, arg2, arg3, alpha){};
+
+// Color Creating & Reading
+Processing.prototype.alpha = function(color){};
+Processing.prototype.blendColor = function(c1, c2, mode){};
+Processing.prototype.blue = function(color){};
+Processing.prototype.brightness = function(color){};
+Processing.prototype.color = function(arg1){};
+Processing.prototype.color = function(arg1, arg2){};
+Processing.prototype.color = function(arg1, arg2, arg3){};
+Processing.prototype.color = function(arg1, arg2, arg3, alpha){};
+Processing.prototype.green = function(color){};
+Processing.prototype.hue = function(color){};
+Processing.prototype.lerpColor = function(c1, c2, amt){};
+Processing.prototype.red = function(color){};
+Processing.prototype.saturation = function(color){};
+
+// PImage
+var PImage = {};
+var PImage = function(){};
+var PImage = function(width, height){};
+var PImage = function(width, height, format){};
+var PImage = function(img){};
+
+PImage.prototype.blend = function(srcImg, x, y, width, height, dx, dy, dwidth, dheight, MODE){};
+PImage.prototype.blend = function(x, y, width, height, dx, dy, dwidth, dheight, MODE){};
+PImage.prototype.updatePixels = function(){};
+PImage.prototype.get = function(x, y, w, h){};
+PImage.prototype.set = function(x, y, c){};
+PImage.prototype.copy = function(srcImg, sx, sy, swidth, sheight, dx, dy, dwidth, dheight){};
+PImage.prototype.copy = function(sx, sy, swidth, sheight, dx, dy, dwidth, dheight){};
+PImage.prototype.filter = function(mode, param){};
+PImage.prototype.filter = function(mode){};
+PImage.prototype.save = function(file){};
+PImage.prototype.resize = function(w, h){};
+PImage.prototype.mask = function(mask){};
+
+Processing.prototype.createImage = function(w, h, mode){};
+
+// PImage Loading & Displaying
+Processing.prototype.image = function(img, x, y){};
+Processing.prototype.image = function(img, x, y, width, height){};
+Processing.prototype.imageMode = function(MODE){};
+Processing.prototype.loadImage = function(filename){};
+Processing.prototype.noTint = function(){};
+Processing.prototype.requestImage = function(filename){};
+Processing.prototype.requestImage = function(filename, extension){};
+Processing.prototype.tint = function(arg1){};
+Processing.prototype.tint = function(arg1, arg2){};
+Processing.prototype.tint = function(arg1, arg2, arg3){};
+Processing.prototype.tint = function(arg1, arg2, arg3, alpha){};
+
+// PImage Pixels is a part of PImage class
+
+// Rendering
+var PGraphics = {};
+var PGraphics = function(){};
+var PGraphics = function(width, height){};
+var PGraphics = function(width, height, applet){};
+
+PGraphics.prototype.beginDraw = function(){};
+PGraphics.prototype.endDraw = function(){};
+
+Processing.prototype.createGraphics = function(width, height, renderer){};
+Processing.prototype.createGraphics = function(width, height, renderer, filename){};
+
+PGraphics.prototype.hint = function(item){};
