1a98c2cc76c5a5960d6d0e177abc1aa3530b8460
Norgat <norg113@gmail.com>
2014-07-09 18:07:16 +0400

    Fix :features initialization time.

--- src/cljs/quil/sketch.clj
+++ src/cljs/quil/sketch.clj
@@ -9,15 +9,14 @@
   [app-name & options]
   (let [raw-opts (apply hash-map options)
         opts     (if (:host raw-opts) raw-opts
-                     (merge raw-opts {:host app-name}))
-        features (set (:features opts))]
+                     (merge raw-opts {:host app-name}))]
     `(do
        (defn ~(vary-meta app-name assoc :export true) []
          (quil.sketch/sketch
-          ~@(apply concat (seq (merge (dissoc opts :features) features)))))
+          ~@(apply concat (seq opts))))
 
-       ~(when (not (:no-start features))
-          `(quil.sketch/add-sketch-to-init-list 
+       (when-not (some #(= :no-start %) ~(:features opts))
+          (quil.sketch/add-sketch-to-init-list 
             {:fn ~app-name 
              :host-id (if (:host raw-opts) 
                           (:host raw-opts)
