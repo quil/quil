12281ccf026a9ee3385d974fc771e2428432c0f5
Norgat <norg113@gmail.com>
2014-07-23 17:11:34 +0400

    Add ClojureScript code version to defsnippet.

--- project.clj
+++ project.clj
@@ -9,13 +9,15 @@
   :license {:name "Common Public License - v 1.0"
             :url "http://www.opensource.org/licenses/cpl1.0"}
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [quil/processing-core "2.2.1"]
                  [quil/processing-pdf "2.2.1"]
                  [quil/processing-dxf "2.2.1"]
                  [quil/jogl-all-fat "2.1.5"]
                  [quil/gluegen-rt-fat "2.1.5"]
-                 [com.lowagie/itext "2.1.7"]]
+                 [com.lowagie/itext "2.1.7"]
+                 [org.clojure/clojurescript "0.0-2268"]
+                 [prismatic/dommy "0.1.2"]]
 
   :plugins [[com.keminglabs/cljx "0.4.0"]]
 
--- test/quil/snippet.clj
+++ test/quil/snippet.clj
@@ -1,16 +1,18 @@
 (ns quil.snippet
-  (:require [quil.core :as q]
+  (:require [quil.util :refer [clj-compilation?]]
+            [quil.core :as q]
             [quil.util :refer [no-fn]]
             [clojure.test :refer [is]]))
 
 (def default-size [500 500])
+(def default-host "quil-test")
 
-(defmacro snippet-as-test [name opts & draw-fn-body]
+(defmacro snippet-as-test [snip-name opts & draw-fn-body]
   `(let [result# (promise)]
      (Thread/sleep 100)
      (q/sketch
-      :title (str '~name)
-      :size default-size
+      :title (str '~snip-name)
+      :size ~(:size opts default-size)
       :renderer ~(:renderer opts :java2d)
       :setup (fn []
                ~(:setup opts)
@@ -26,15 +28,42 @@
       :on-close #(deliver result# nil))
      (is (nil? @result#))))
 
-(defmacro defsnippet [name opts & body]
-  `(def ~(vary-meta name assoc
-                    :test `(fn [] (snippet-as-test ~name ~opts ~@body)))
+(defmacro defsnippet [snip-name opts & body]
+  (if (clj-compilation?)
+    ;; Clojure version
+    `(def ~(vary-meta snip-name assoc
+                    :test `(fn [] (snippet-as-test ~snip-name ~opts ~@body)))
      (fn []
        (q/sketch
-        :title (str '~name)
+        :title (str '~snip-name)
         :size ~default-size
         :setup (fn []
                  (q/frame-rate 5)
                  ~(:setup opts))
         :renderer ~(:renderer opts :java2d)
-        :draw (fn [] ~@body)))))
+        :draw (fn [] ~@body))))
+
+    ;; ClojureScript version
+    `(do
+     (defn ~snip-name []
+       (quil.core/sketch
+        :size ~(:size opts default-size)
+        :renderer ~(:renderer opts :p2d)
+        :host ~(:host opts default-host)
+
+        :setup (fn []
+                 ~(:setup opts)
+                 (q/frame-rate 5))
+
+        :draw (fn []
+                (try
+                  ~@body
+                  (catch js/Error e#
+                    (swap! quil-test.core/failed inc)
+                    (throw (js/Error. e#)))
+                  (finally (q/exit))))))
+
+     (swap! quil-test.core/test-data conj
+            {:name (name '~snip-name)
+             :ns ~(str (ns-name *ns*))
+             :fn ~snip-name}))))
