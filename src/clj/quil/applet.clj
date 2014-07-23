56b11553ed2dfd0875e0a2c7cacd3f8fa9466530
Norgat <norg113@gmail.com>
2014-07-23 14:59:58 +0400

    Add support of :display option.

--- src/clj/quil/applet.clj
+++ src/clj/quil/applet.clj
@@ -77,10 +77,11 @@
   (PApplet/runSketch
    (into-array String
                (vec (filter string?
-                   [(when (and (:bgcolor (meta applet))
-                               (:present (meta applet)))
-                     (str "--bgcolor" "=" (str (:bgcolor (meta applet)))))
-                    "--hide-stop" title])))
+                            [(str "--display=" (:display (meta applet)))
+                             (when (and (:bgcolor (meta applet))
+                                        (:present (meta applet)))
+                               (str "--bgcolor" "=" (str (:bgcolor (meta applet)))))
+                             "--hide-stop" title])))
    applet)
   (prepare-applet-frame applet title))
 
@@ -105,21 +106,27 @@
 (defn- display-size
   "Returns size of screen. If there are 2 or more screens it probably return size of
   default one whatever it means."
-  []
-  (let [bounds (.. (java.awt.GraphicsEnvironment/getLocalGraphicsEnvironment)
-                   getDefaultScreenDevice
-                   getDefaultConfiguration
-                   getBounds)]
-    [(.-width bounds) (.-height bounds)]))
+  ([] (display-size 0))
+
+  ([display]
+   (let [devices (.. (java.awt.GraphicsEnvironment/getLocalGraphicsEnvironment)
+                     getScreenDevices)]
+     (if (and (>= display 0) (< display (alength devices)))
+       (let [bounds (.. (aget devices display)
+                        getDefaultConfiguration
+                        getBounds)]
+         [(.-width bounds) (.-height bounds)])
+       (throw (IllegalArgumentException.
+                      (str "Invalid display definition: " display ". Displays are numbered starting from 0")))))))
 
 (defn- process-size
   "Checks that the size vector is exactly two elements. If not, throws
   an exception, otherwise returns the size vector unmodified."
-  [size]
-  (cond (= size :fullscreen) (display-size)
+  [size display]
+  (cond (= size :fullscreen) (display-size display)
         (and (coll? size) (= 2 (count size))) size
         :else (throw (IllegalArgumentException.
-                      (str "Invalid size definition:" size ". Was expecting :fullscreen or 2 elements vector: [x-size y-size].")))))
+                      (str "Invalid size definition: " size ". Was expecting :fullscreen or 2 elements vector: [x-size y-size].")))))
 
 (defn- to-method-name [keyword]
   "Converts keyword to java-style method symbol. :on-key-pressed => onKeyPressed"
@@ -304,6 +311,8 @@
                      Color is specified in hex format: #XXXXXX.
                      Example: :bgcolor \"#00FFFF\" (cyan background)
 
+   :display        - Set what display should be used by this sketch. Displays are numbered starting from 0.
+
    :setup          - A function to be called once when setting the sketch up.
 
    :draw           - A function to be repeatedly called at most n times per
@@ -366,10 +375,12 @@
                       options
                       (safe-fns options))
 
+        display           (or (:display options) 0)
+
         options           (merge (dissoc options :features)
                                  features)
 
-        size              (process-size (:size options))
+        size              (process-size (:size options) display)
         title             (or (:title options) (str "Quil " (swap! untitled-applet-id* inc)))
         renderer          (or (:renderer options) :java2d)
         draw-fn           (or (:draw options) no-fn)
@@ -394,6 +405,7 @@
                                   :draw-fn draw-fn
                                   :renderer renderer
                                   :size size
+                                  :display display
                                   :target-frame-rate (atom 60)}
                                  listeners)
         prx-obj           (quil.Applet. applet-state)]
