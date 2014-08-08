82bfb8b2d7fed9273be0078769b561c1a9522299
Norgat <norg113@gmail.com>
2014-08-08 17:18:53 +0400

    Fix pathes and routes.

--- test/clj/test_server.clj
+++ test/clj/test_server.clj
@@ -1,13 +1,14 @@
 (ns test-server
-  (:require [compojure.core :refer :all]
+  (:require ring.util.response
+            [compojure.core :refer :all]
             [compojure.handler :as handler]
             [compojure.route :as route]
             [clj-http.client :as client]))
 
-
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/resources "/")
+  (compojure.core/GET "/" req (ring.util.response/redirect "/test.html"))
+  (route/files "/js" {:root "target/js"})
+  (route/files "/" {:root "test/html"})
   (route/not-found "Not Found"))
 
 (def app
