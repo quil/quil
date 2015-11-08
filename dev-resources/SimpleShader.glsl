935279329515c9d9919b8a99774153b9d840dcd9
Nikita Beloglazov <nikelandjelo@gmail.com>
2015-11-07 21:47:16 -0800

    Update shader for tests to make obvious when it works. It's edge-detection shader and should draw borders around edges.

--- dev-resources/SimpleShader.glsl
+++ dev-resources/SimpleShader.glsl
@@ -1,7 +1,40 @@
-uniform vec4 ambient_color;
-uniform float ambient_intensity;
+#ifdef GL_ES
+precision mediump float;
+precision mediump int;
+#endif
 
-void main(void)
-{
-  gl_FragColor = ambient_color * ambient_intensity/2.0;
+#define PROCESSING_TEXTURE_SHADER
+
+uniform sampler2D texture;
+uniform vec2 texOffset;
+
+varying vec4 vertColor;
+varying vec4 vertTexCoord;
+
+const vec4 lumcoeff = vec4(0.299, 0.587, 0.114, 0);
+
+void main() {
+  vec2 tc0 = vertTexCoord.st + vec2(-texOffset.s, -texOffset.t);
+  vec2 tc1 = vertTexCoord.st + vec2(         0.0, -texOffset.t);
+  vec2 tc2 = vertTexCoord.st + vec2(+texOffset.s, -texOffset.t);
+  vec2 tc3 = vertTexCoord.st + vec2(-texOffset.s,          0.0);
+  vec2 tc4 = vertTexCoord.st + vec2(         0.0,          0.0);
+  vec2 tc5 = vertTexCoord.st + vec2(+texOffset.s,          0.0);
+  vec2 tc6 = vertTexCoord.st + vec2(-texOffset.s, +texOffset.t);
+  vec2 tc7 = vertTexCoord.st + vec2(         0.0, +texOffset.t);
+  vec2 tc8 = vertTexCoord.st + vec2(+texOffset.s, +texOffset.t);
+  
+  vec4 col0 = texture2D(texture, tc0);
+  vec4 col1 = texture2D(texture, tc1);
+  vec4 col2 = texture2D(texture, tc2);
+  vec4 col3 = texture2D(texture, tc3);
+  vec4 col4 = texture2D(texture, tc4);
+  vec4 col5 = texture2D(texture, tc5);
+  vec4 col6 = texture2D(texture, tc6);
+  vec4 col7 = texture2D(texture, tc7);
+  vec4 col8 = texture2D(texture, tc8);
+
+  vec4 sum = 8.0 * col4 - (col0 + col1 + col2 + col3 + col5 + col6 + col7 + col8); 
+  gl_FragColor = vec4(sum.rgb, 1.0) * vertColor; 
 }
+
