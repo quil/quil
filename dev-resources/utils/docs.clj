3d89cf3a38ab2c1f45551e0a1c83ab3725b8b688
Anthony Galea <anthony.galea@gmail.com>
2019-04-02 09:49:01 +0200

    Fix links in markdown generated for quil-site.

--- dev-resources/utils/docs.clj
+++ dev-resources/utils/docs.clj
@@ -14,6 +14,22 @@
            (com.vladsch.flexmark.ext.wikilink WikiLinkExtension)
            (com.vladsch.flexmark.util.options MutableDataSet)))
 
+(defn as-url [str]
+  (-> str
+      (string/lower-case)
+      (string/replace  #"[ &,?]" {" " "-"
+                                  "," ""
+                                  "&" "and"
+                                  "?" "_q"})))
+
+(defn wiki-link
+  [function-name category subcategory]
+  (str "/api"
+       (when category (str "/" (as-url category)))
+       (when subcategory (str "/" (as-url subcategory)))
+       "#"
+       function-name))
+
 (def md-extensions
   [(TablesExtension/create)
    (AutolinkExtension/create)
@@ -27,7 +43,7 @@
 
 (defn- md-renderer
   "Create a Markdown renderer."
-  []
+  [function->category-info]
   (.. (HtmlRenderer/builder
        (doto (MutableDataSet.)
          (.set AnchorLinkExtension/ANCHORLINKS_ANCHOR_CLASS "md-anchor")
@@ -41,19 +57,21 @@
             (reify LinkResolver
               (resolveLink [_this _node _ctx link]
                 (if (= (.getLinkType link) WikiLinkExtension/WIKI_LINK)
-                  (ResolvedLink. LinkType/LINK
-                                 (.getUrl link)
+                  (let [function-name (.getUrl link)
+                        {:keys [category subcategory]} (get function->category-info function-name)]
+                    (ResolvedLink. LinkType/LINK
+                                 (wiki-link function-name category subcategory)
                                  nil
-                                 LinkStatus/UNCHECKED)
+                                 LinkStatus/UNCHECKED))
                   link))))))
       (extensions md-extensions)
       (build)))
 
 (defn markdown-to-html
   "Parse the given string as Markdown and return HTML."
-  [markdown]
+  [markdown function->category-info]
   (->> (.parse md-container markdown)
-       (.render (md-renderer))))
+       (.render (md-renderer function->category-info))))
 
 (defn read-all [file feature]
   (let [reader (->> file
@@ -70,7 +88,7 @@
   (filter #(-> % second meta (contains? :category))
           all-forms))
 
-(defn get-full-meta [[type name docstring args :as form]]
+(defn get-full-meta [function->category-info [type name docstring args :as form]]
   (let [mt (meta name)
         args (if (vector? args)
                #{args}
@@ -78,17 +96,35 @@
     (assoc mt
            :args args
            :name name
-           :docstring (markdown-to-html docstring)
+           :docstring (markdown-to-html docstring function->category-info)
            :what (if (= 'defmacro type) :macro :fn)
            :link (link-to-processing-reference mt))))
 
+(defn get-category-info [[type symbol docstring args :as form]]
+  [(name symbol)
+   (select-keys (meta symbol) [:category :subcategory])])
+
+(def files ["quil/core.cljc" "quil/middleware.cljc"])
+
+(defn get-function->category-info []
+  (let [clj (->> (mapcat #(read-all % :clj) files)
+                get-public-fns
+                (map get-category-info)
+                (into {}))
+        cljs (->> (mapcat #(read-all % :cljs) files)
+                 get-public-fns
+                 (map get-category-info)
+                 (into {}))]
+    (merge clj cljs)))
+
 (defn get-metas [forms type]
-  (->> forms
-       get-public-fns
-       (map get-full-meta)
-       (map #(assoc % :type type))
-       (map #(vector (:name %) %))
-       (into {})))
+  (let [function->category-info (get-function->category-info)]
+    (->> forms
+         get-public-fns
+         (map #(get-full-meta function->category-info %))
+         (map #(assoc % :type type))
+         (map #(vector (:name %) %))
+         (into {}))))
 
 (defn merge-metas [meta1 meta2]
   (let [args1 (:args meta1)
@@ -104,8 +140,6 @@
 
 (defn merge-all-metas [metas1 metas2]
   (merge-with merge-metas metas1 metas2))
-
-(def files ["quil/core.cljc" "quil/middleware.cljc"])
 
 (defn generate-metas-to-file [file]
   (let [clj (-> (mapcat #(read-all % :clj) files)
