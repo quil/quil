5e434525bab26e32c456b9cdff1dd462f697e738
Anthony Galea <anthony.galea@gmail.com>
2019-04-11 08:50:51 +0200

    Add test to check that all p5js-names are valid.

--- test/clj/fn_metas.clj
+++ test/clj/fn_metas.clj
@@ -1,10 +1,12 @@
 (ns fn-metas
   (:require quil.core
             quil.middleware
-            [quil.helpers.docs :refer [link-to-processing-reference]]
-            [clojure.string :refer [split]]
+            [quil.helpers.docs :refer [link-to-processing-reference
+                                       link-to-p5js-reference]]
+            [clojure.string :as string]
             [clojure.test :refer [deftest is]]
-            [clj-http.client :as http]))
+            [clj-http.client :as http]
+            [cheshire.core :as json]))
 
 (defn get-public-fns-metas []
   (mapcat #(->> % the-ns ns-publics vals (map meta))
@@ -26,8 +28,30 @@
                                               :valid? (valid-link? link)})))]
     (is valid? (str "Link " link " is not valid"))))
 
+(defn p5js-method-or-property? [item]
+  (->> (get item "itemtype")
+       (contains? #{"method" "property"})))
+
+(defn p5js-method-names []
+  (let [items (-> (slurp "https://p5js.org/reference/data.min.json")
+                  (json/parse-string)
+                  (get "classitems"))
+        method-names (->> items
+                          (filter p5js-method-or-property?)
+                          (map #(str (get % "class") "." (get % "name")))
+                          (map #(string/replace % "p5." "")))]
+    (into #{} method-names)))
+
+(deftest p5js-names-valid
+  (let [p5js-names (p5js-method-names)]
+    (doseq [p5js-name (->> (get-public-fns-metas)
+                           (map :p5js-name)
+                           (remove nil?)
+                           (map #(string/replace % "()" "")))]
+      (is (contains? p5js-names p5js-name)))))
+
 (defn max-docstring-length [docstring]
-  (->> (split docstring #"\n")
+  (->> (string/split docstring #"\n")
        (map count)
        (apply max)))
 
