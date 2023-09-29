4178884411e5aabb1625f4d3ffd5d9e9009fc799
Charles Comstock <dgtized@gmail.com>
2023-09-29 13:57:48 -0500

    add example versions to verify browser runner
    but leave commented out so tests pass

--- test/quil/test_runner.cljs
+++ test/quil/test_runner.cljs
@@ -1,11 +1,17 @@
 (ns quil.test-runner
   (:require
-   [cljs.test]
+   #_[clojure.test :as t :refer [deftest is] :include-macros true]
    [figwheel.main.testing :refer-macros [run-tests-async]]
 
    ;; require namespaces to tests
    quil.calculation-test
    ))
+
+;; uncomment to verify browser version
+#_(deftest verify-browser
+    (is (empty? (.-userAgent js/navigator)))
+    (is (empty? (.-appName js/navigator)))
+    (is (empty? (.-appVersion js/navigator))))
 
 (defn -main [& _args]
   (run-tests-async 5000))
