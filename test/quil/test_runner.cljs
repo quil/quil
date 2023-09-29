7669e6496156ac486cb53dae99a047d41ae059c2
Charles Comstock <dgtized@gmail.com>
2023-09-29 13:41:20 -0500

    bare bones async-test setup, primarily for run in CI

--- test/quil/test_runner.cljs
+++ test/quil/test_runner.cljs
@@ -1,44 +1,11 @@
-(ns ^:figwheel-hooks quil.test-runner
+(ns quil.test-runner
   (:require
-   [cljs-test-display.core :as td]
-   [figwheel.main.testing :refer [run-tests-async run-tests]]
-   [fipp.edn :as fedn]
-   [goog.dom :as gdom]
+   [cljs.test]
+   [figwheel.main.testing :refer-macros [run-tests-async]]
 
    ;; require namespaces to tests
    quil.calculation-test
    ))
-
-(enable-console-print!)
-
-(defn prettier [content]
-  (td/n :pre {}
-        (td/n :code {} (with-out-str (fedn/pprint content {:width 100})))))
-
-;; modified from https://github.com/bhauman/cljs-test-display/issues/5#issuecomment-619090019
-;; pretty print expected vs actual with fedn/pprint
-(set! td/comparison
-      (fn comparison [{:keys [expected actual]}]
-        (td/div
-         (prettier expected)
-         (td/div :actual-x (prettier actual)))))
-
-(def style-overrides
-  "pre {overflow-x: auto;}
-.actual-x {border-top: 1.5px dashed rgb(236 196 196);}
-")
-
-;; to view, visit http://localhost:9600/figwheel-extra-main/tests
-(defn ^:after-load test-run []
-  (run-tests (cljs-test-display.core/init! "app-tests"))
-  (when-not (gdom/getElement "style-override")
-    (let [head (aget (gdom/getElementsByTagName "head") 0)]
-      (gdom/appendChild head
-                        (td/n :style {:id "style-override"}
-                              style-overrides)))))
-
-(defonce initialize-page
-  (test-run))
 
 (defn -main [& _args]
   (run-tests-async 5000))
