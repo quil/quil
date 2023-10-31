9efb26a601192b2396d2552ce93c6ba4b4757946
Charles Comstock <dgtized@gmail.com>
2023-10-31 10:37:29 -0500

    Expand on cljs resizing test
    Initially on the default canvas everything is scaled correctly, with the lines
    at the halfway mark horizontal and vertical and the ellipse in the upper left
    quadrant. However, after resizing, the sketch canvas is quartered. The
    resolution is correct, but it only displays the upper left quadrant.

--- test/cljs/quil/manual.cljs
+++ test/cljs/quil/manual.cljs
@@ -189,13 +189,22 @@
    :size [500 500]
    :draw (fn []
            (q/background 240)
-           (q/text-size 30)
+           (q/text-size 20)
            (q/fill 0)
-           (q/text-align :center :center)
+           (q/text-align :left :top)
            (q/text (str "width: " (q/width)
-                        "\nheight: " (q/height))
-                   (/ (q/width) 2)
-                   (/ (q/height) 2)))))
+                        "\nheight: " (q/height)
+                        "\ndensity: " (q/display-density))
+                   10 10)
+
+           ;; FIXME: after resize cross hairs are on lower, right edges because
+           ;; width/height of canvas element changes from style width/height.
+           ;; initial canvas has width/height 1000x1000, but after resize it is
+           ;; 700x700 and 500x500 but only showing the upper left quadrant of
+           ;; the canvas.
+           (q/ellipse (/ (q/width) 4) (/ (q/height) 4) 10 10)
+           (q/line 0 (/ (q/height) 2) (q/width) (/ (q/height) 2))
+           (q/line (/ (q/width) 2) 0 (/ (q/width) 2) (q/height)))))
 
 (defn resize-sketch []
   (.setTimeout js/window
