528e7a2563359125ea67e225a4b56d4e6f197302
Charles Comstock <dgtized@gmail.com>
2023-10-31 10:54:59 -0500

    share common resizing test between fullscreen and manual resize

--- test/cljs/quil/fullscreen.cljs
+++ test/cljs/quil/fullscreen.cljs
@@ -1,22 +1,10 @@
 (ns quil.fullscreen
-  (:require [quil.core :as q :include-macros true]
-            [quil.middlewares.fun-mode :as fm]
+  (:require [quil.examples :as qe]
             [goog.events :as events]
             [goog.events.EventType :as EventType]))
 
 (defn start-sketch []
-  (q/sketch
-   :host "fullscreen"
-   :size [500 500]
-   :draw (fn []
-           (q/background 240)
-           (q/text-size 30)
-           (q/fill 0)
-           (q/text-align :center :center)
-           (q/text (str "width: " (q/width)
-                        "\nheight: " (q/height))
-                   (/ (q/width) 2)
-                   (/ (q/height) 2)))))
+  (qe/resize "fullscreen"))
 
 (events/listenOnce js/window EventType/LOAD
                    #(when (= (-> js/document
--- test/cljs/quil/manual.cljs
+++ test/cljs/quil/manual.cljs
@@ -1,5 +1,6 @@
 (ns quil.manual
   (:require [quil.core :as q :include-macros true]
+            [quil.examples :as qe]
             [quil.middlewares.fun-mode :as fm]
             [goog.events :as events]
             [goog.events.EventType :as EventType]))
@@ -184,27 +185,8 @@
            (q/fill 0 0 0)
            (q/text (str "mouse pressed: "  (q/mouse-pressed?)) 0 20)
            (q/text (str "key pressed: " (q/key-pressed?)) 0 40)))
-  (q/sketch
-   :host "resizing"
-   :size [500 500]
-   :draw (fn []
-           (q/background 240)
-           (q/text-size 20)
-           (q/fill 0)
-           (q/text-align :left :top)
-           (q/text (str "width: " (q/width)
-                        "\nheight: " (q/height)
-                        "\ndensity: " (q/display-density))
-                   10 10)
 
-           ;; FIXME: after resize cross hairs are on lower, right edges because
-           ;; width/height of canvas element changes from style width/height.
-           ;; initial canvas has width/height 1000x1000, but after resize it is
-           ;; 700x700 and 500x500 but only showing the upper left quadrant of
-           ;; the canvas.
-           (q/ellipse (/ (q/width) 4) (/ (q/height) 4) 10 10)
-           (q/line 0 (/ (q/height) 2) (q/width) (/ (q/height) 2))
-           (q/line (/ (q/width) 2) 0 (/ (q/width) 2) (q/height)))))
+  (qe/resize "resizing"))
 
 (defn resize-sketch []
   (.setTimeout js/window
