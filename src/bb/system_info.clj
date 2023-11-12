e802f14a17a4f6af713d4ed79bb6914cdf5a8609
Charles Comstock <dgtized@gmail.com>
2023-11-12 17:48:22 -0600

    attempt to differentiate if clojure for bb system-info

--- src/bb/system_info.clj
+++ src/bb/system_info.clj
@@ -4,10 +4,12 @@
 
 (defn versions []
   (println "$ uname -a")
-  (bp/shell "uname -a")
+  (bp/shell "uname" "-a")
   (println "$ java -version")
-  (bp/shell "java -version")
+  (bp/shell "java" "-version")
   (println "$ bb version")
-  (bp/shell "bb version")
-  (println "$ clojure -version")
-  (bp/shell "clojure -version"))
+  (bp/shell "bb" "version")
+  (let [cmd (if (= (System/getenv "RUNNER_OS") "Windows")
+              "clj" "clojure")]
+    (println (str "$ " cmd " -version"))
+    (bp/shell cmd "-version")))
