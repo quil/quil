d834176d410ceff8b6c96c1f83f6e14d6dc0942c
Charles Comstock <dgtized@gmail.com>
2023-12-07 16:00:30 -0600

    loosely document snippet-test examples

--- test/quil/snippet_test.clj
+++ test/quil/snippet_test.clj
@@ -5,6 +5,9 @@
    [quil.snippets.all-snippets :as as]
    [quil.test-util :as tu]))
 
+;; run only this namespace:
+;; clj -X:test :nses '[quil.snippet-test]'
+
 (defn verify-expected-snapshot-exists [{:keys [ns name skip-image-diff?]}]
   (when-not skip-image-diff?
     (doseq [platform ["clj" #_"cljs"] ;; 28 missing cljs cases
@@ -13,13 +16,16 @@
             (str "Missing expected image " expected " for "
                  ns " " name)))))
 
+;; Snippet tests compare against a reference image, this ensures all the
+;; expected reference images exists
 (t/deftest expected-images-exist
   (doseq [snippet as/all-snippets]
     (verify-expected-snapshot-exists snippet)))
 
+;; Snippets :fns key describes the list of quil.core functions it depends on, so
+;; this test verifies the functions exist.
 (t/deftest all-snippets-map-to-function
   (doseq [{:keys [fns name]} as/all-snippets
           fn fns]
     (t/is (some? (ns-resolve 'quil.core (symbol fn)))
           (str "Snippet '" name "' matches to non-existent function '" fn "'"))))
-
