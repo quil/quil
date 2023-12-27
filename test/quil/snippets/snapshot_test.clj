7c0a06d894cf3c9e355d4756e6dedb11dfacc46e
Charles Comstock <dgtized@gmail.com>
2023-12-27 15:46:18 -0600

    re-enable random tests but slow down to 4fps

--- test/quil/snippets/snapshot_test.clj
+++ test/quil/snippets/snapshot_test.clj
@@ -39,7 +39,7 @@
               ;; only slow down frame rate during manual runs or CI
               ;; in CI the hope is this will reduce SEGV problems
               (when (or manual? github-actions?)
-                (q/frame-rate 5)))
+                (q/frame-rate 4)))
      :mouse-clicked (:mouse-clicked snippet)
      :settings (fn [] (when-let [settings (:settings opts)]
                        (settings)))
