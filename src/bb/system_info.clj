d5c5b97a573b2562dad2e223cd374d7b45f1be95
Charles Comstock <dgtized@gmail.com>
2025-07-11 17:52:07 -0500

    install imagemagick so test comparison runs
    also report imagemagick versions with system-info

--- src/bb/system_info.clj
+++ src/bb/system_info.clj
@@ -11,4 +11,9 @@
   (bp/shell "bb" "version")
   (when-not (= (System/getenv "RUNNER_OS") "Windows")
     (println "$ clojure -version")
-    (bp/shell "clojure" "-version")))
+    (bp/shell "clojure" "-version"))
+
+  ;; imagemagick utilities for image compare
+  (doseq [pname ["compare" "convert" "identify"]]
+    (println "$" pname "-version")
+    (bp/shell (str pname " -version"))))
